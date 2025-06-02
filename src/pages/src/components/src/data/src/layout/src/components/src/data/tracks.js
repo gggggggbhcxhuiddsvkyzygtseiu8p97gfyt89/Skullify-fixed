49d8a291579b8e88cc2522cbcfa59268260c4b7a
gggggggbhcxhuiddsvkyzygtseiu8p97gfyt89 <167583793+gggggggbhcxhuiddsvkyzygtseiu8p97gfyt89@users.noreply.github.com>
2025-06-02 09:50:46 -0700

    Create tracks.js

---
+++ src/pages/src/components/src/data/src/layout/src/components/src/data/tracks.js
@@ -0,0 +1,25 @@
+const tracks = [
+  {
+    id: 1,
+    title: "Alan Walker - Faded",
+    artist: "Alan Walker",
+    url: "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    cover: "https://i1.sndcdn.com/artworks-000145295881-qqzdr5-t500x500.jpg"
+  },
+  {
+    id: 2,
+    title: "The Weeknd - Blinding Lights",
+    artist: "The Weeknd",
+    url: "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+    cover: "https://upload.wikimedia.org/wikipedia/en/thumb/0/09/The_Weeknd_-_Blinding_Lights.png/220px-The_Weeknd_-_Blinding_Lights.png"
+  },
+  {
+    id: 3,
+    title: "Imagine Dragons - Believer",
+    artist: "Imagine Dragons",
+    url: "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    cover: "https://i1.sndcdn.com/artworks-000221154626-6q72z0-t500x500.jpg"
+  }
+];
+
+export default tracks;
